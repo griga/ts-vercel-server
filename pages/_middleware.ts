4b8007af6f7ff74c5e3c6eaeb8b09d238299b93d
griga <grigach@gmail.com>
2022-03-09 01:16:36 +0200

    logs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,6 +5,8 @@ export default function middleware(
   event: NextFetchEvent
 ) {
   console.log(JSON.stringify(request.geo));
+  console.log(JSON.stringify(request.ip));
+  console.log(JSON.stringify(request.ua));
   console.log(JSON.stringify(request.headers));
   return new Response("Hello, world!");
 }
