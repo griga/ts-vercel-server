7ef6ab5f837d1748921fc48d417279628cbe30c9
griga <grigach@gmail.com>
2022-03-08 22:06:54 +0200

    fix types

--- api/echo-id.ts
+++ api/echo-id.ts
@@ -1,10 +1,10 @@
-import { NowRequest, NowResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 type QueryParams = {
   id: string;
 };
 
-export default async (req: NowRequest, res: NowResponse) => {
+export default async (req: VercelRequest, res: VercelResponse) => {
   const { query } = req;
   if (!_isValidVercelRequest(query)) {
     res.json({ error: "error" });
