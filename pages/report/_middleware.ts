b99bff1898f2559397c475eda5fba8275c7baee3
griga <grigach@gmail.com>
2022-03-09 04:11:50 +0200

    report

--- pages/report/_middleware.ts
+++ pages/report/_middleware.ts
@@ -4,11 +4,10 @@ export default function middleware(
   request: NextRequest,
   event: NextFetchEvent
 ) {
+  console.log(request.nextUrl.href);
   console.log(JSON.stringify(request.geo));
   console.log(JSON.stringify(request.ip));
   console.log(JSON.stringify(request.ua));
   console.log(JSON.stringify(request.headers));
-  return NextResponse.json({
-    message: 'success'
-  })
+  return NextResponse.next()
 }
